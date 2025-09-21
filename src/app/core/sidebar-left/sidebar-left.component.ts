2b712a2c627cd6a35051a6ea139611357f9b384b
Kanchan Rajput <designbuilderx@gmail.com>
2025-09-21 15:51:03 +0530

    adjusted the height of the side navigation

--- src/app/core/sidebar-left/sidebar-left.component.ts
+++ src/app/core/sidebar-left/sidebar-left.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PreviewService } from '../../services/preivew';
 import { Router } from '@angular/router';
@@ -12,20 +12,14 @@ import { componentsTree } from '../../backendData/category-component-tree';
   styleUrls: ['./sidebar-left.component.scss'],
 })
 export class SidebarLeftComponent {
+  @Input() drawerOpen = false;
+  @Output() closeDrawer = new EventEmitter<void>();
+
   frameworks = ['Bootstrap', 'Tailwind', 'Material', 'Vanilla', 'Ant Design'];
   selectedFramework = 'Bootstrap';
   componentsTree = componentsTree;
 
-  drawerOpen = false; // For mobile drawer
-
-  constructor(
-    private previewService: PreviewService,
-    private route: Router
-  ) {}
-
-  toggleDrawer() {
-    this.drawerOpen = !this.drawerOpen;
-  }
+  constructor(private previewService: PreviewService, private route: Router) {}
 
   toggleCategory(category: any) {
     category.expanded = !category.expanded;
@@ -33,6 +27,7 @@ export class SidebarLeftComponent {
 
   selectFramework(framework: string) {
     this.selectedFramework = framework;
+    this.closeDrawer.emit(); // close drawer on mobile
   }
 
   selectComponent(component: any, categoryName: string) {
@@ -44,7 +39,6 @@ export class SidebarLeftComponent {
     this.previewService.setComponent(compWithName);
     this.previewService.setPreview(compWithName.children[0].html);
     this.route.navigate(['/home']);
-    this.drawerOpen = false; // Close drawer on mobile after selection
+    this.closeDrawer.emit(); // close drawer on mobile
   }
 }
-
